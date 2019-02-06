c82ef17974a338355da2a3887c87976d9b6af147
Ayana.shahab <37789259+AyanaShb@users.noreply.github.com>
2019-02-06 11:06:18 +0700

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -1,66 +1,96 @@
-
 window.onload = function (e) {
     liff.init(function () {
-        initializeApp();
-    });
-    $('button').hover(function(){
-    $('.glitch-wrapper').toggleClass('paused');
-    $('body').toggleClass('paused');
+        getP();
     });
 };
 
-function initializeApp(){
-    var type = getParameterByName('type')
-    if (type=== "text") {
-        liff.sendMessages([{type: 'text',text: getParameterByName('text')}]).then(function () {liff.closeWindow()});
-    }else if(type=="sticker"){
-        var stk = getParameterByName('tstk');
-        var sid = getParameterByName('stkid');
-        var pkg = getParameterByName('stkpkgid');
-        var send = getParameterByName('send');
-        var uriz="line://shop/sticker/detail/"+pkg;
-        var tp="";
-        var ep = '';
-        if (stk === 'animasi') {
-            ep = "https://stickershop.line-scdn.net/stickershop/v1/sticker/"+sid+"/IOS/sticker_animation@2x.png";
-            tp="a";
-        } else {
-            ep = "https://stickershop.line-scdn.net/stickershop/v1/sticker/"+sid+"/IOS/sticker@2x.png";
-            tp="n";
-        }
-        if (send===null){
-        }else{
-            if(send===true || send==="true")
-                if(tp==="a"){
-                    uriz="line://app/1600328768-y3yq64nw/?type=sticker&tstk=animasi&stkid="+sid+"&stkpkgid="+pkg+"&send=true";
-                }else{
-                    uriz="line://app/1600328768-y3yq64nw/?type=sticker&tstk=anime&stkid="+sid+"&stkpkgid="+pkg+"&send=true";
-                }
-        }
+function getP(){
+    var tipe = getParameterByName('type')
+    if (!tipe) {
+        document.getElementById('textx').addEventListener('click', function () {
+            liff.sendMessages([{
+                type: 'text',
+                text: 'line://app/1643532430-zdredkAG?type=text&text=Your%20text\n\ntype => text\ntext => your text'
+            }]).then(function () {
+                liff.closeWindow();
+            });
+        });
+        document.getElementById('imagex').addEventListener('click', function () {
+            liff.sendMessages([{
+                type: 'text',
+                text: 'line://app/1643532430-zdredkAG?type=image&img=https://wallpaperstudio10.com/static/wpdb/wallpapers/1000x563/168891.jpg\n\ntype => image\nimg => Link (must be HTTPS)'
+            }]).then(function () {
+                liff.closeWindow();
+            });
+        });
+        document.getElementById('videox').addEventListener('click', function () {
+            liff.sendMessages([{
+                type: 'text',
+                text: 'line://app/1643532430-zdredkAG?type=video&ocu=https://tinyurl.com/y8og3or5&piu=https://images6.alphacoders.com/710/thumb-350-710132.png\n\ntype => video\nocu => video url\npiu => preview image'
+            }]).then(function () {
+                liff.closeWindow();
+            });
+        });
+        document.getElementById('audiox').addEventListener('click', function () {
+            liff.sendMessages([{
+                type: 'text',
+                text: 'line://app/1643532430-zdredkAG?type=audio&link=https://platelets.fun/public/sounds/music.mp3'
+            }]).then(function () {
+                liff.closeWindow();
+            });
+        });
+        document.getElementById('stickerx').addEventListener('click', function () {
+            liff.sendMessages([{
+                type: 'text',
+                text: 'With Animation: \nline://app/1643532430-zdredkAG?type=sticker&stk=anim&sid=32128231&pkg=3099312\n\nNo Animation: \nline://app/1643612142-oBOXxnOW?type=sticker&stk=noanim&sid=32128231&pkg=3099312\n\ntype => sticker\nstk => anim / noanim\nsid => sticker id\npkg => packages id'
+            }]).then(function () {
+                liff.closeWindow();
+            });
+        });
+    } else {
+        makeText();
+        makeImage();
+        makeVideo();
+        makeAudio();
+        makeSticker();
+        meProfile();
+    }
+    }
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+function getProfile(){
+    liff.getProfile().then(function (profile) {
+        document.getElementById('userid').textContent = 'Hai  ' + profile.displayName;
+        document.getElementById('main').src = profile.pictureUrl;
+        document.getElementById('close').addEventListener('click', function () {
+            liff.closeWindow();
+        });
+    });
+}
+
+function makeText(){
+    var tipe = getParameterByName('type');
+    if (tipe === 'text') {
         liff.sendMessages([{
-          type: "template",
-          altText: "Sticker",
-          template: {
-             type: "image_carousel",
-             columns: [{
-                 imageUrl: ep,
-                 action: {
-                     type: "uri",
-                     uri: uriz}}
-                          ]
-                        }
+            type: 'text',
+            text: getParameterByName('text')
         }]).then(function () {
             liff.closeWindow();
         });
-    }else if (type === 'audio') {
-        liff.sendMessages([{
-            type: 'audio',
-            originalContentUrl: getParameterByName('link'),
-            duration: 60000
-        }]).then(function () {
-            liff.closeWindow();
-        });
-    }else if (type === 'image') {
+    }
+}
+
+function makeImage(){
+    var tipe = getParameterByName('type');
+    if (tipe === 'image') {
         liff.sendMessages([{
             type: 'image',
             originalContentUrl: getParameterByName('img'),
@@ -68,7 +98,12 @@ function initializeApp(){
         }]).then(function () {
             liff.closeWindow();
         });
-    }else if (type === 'video') {
+    }
+}
+
+function makeVideo(){
+    var tipe = getParameterByName('type');
+    if (tipe === 'video') {
         liff.sendMessages([{
             type: 'video',
             originalContentUrl: getParameterByName('ocu'),
@@ -78,12 +113,82 @@ function initializeApp(){
         });
     }
 }
-function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, '\\$&');
-    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+
+function makeAudio(){
+    var tipe = getParameterByName('type');
+    if (tipe === 'audio') {
+        liff.sendMessages([{
+            type: 'audio',
+            originalContentUrl: getParameterByName('link'),
+            duration: 60000
+        }]).then(function () {
+            liff.closeWindow();
+        });
+    }
+}
+
+function makeSticker(){
+    var tipe = getParameterByName('type');
+    if (tipe === 'sticker') {
+        var stk = getParameterByName('stk');
+        var sid = getParameterByName('sid');
+        var pkg = getParameterByName('pkg');
+        var ep = '';
+        if (stk === 'anim') {
+            ep = "/IOS/sticker_animation@2x.png";
+        } else {
+            ep = "/IOS/sticker@2x.png";
+        }
+        liff.sendMessages([{
+          type: "template",
+          altText: "Sticker",
+          template: {
+             type: "image_carousel",
+             columns: [{
+                 imageUrl: "https://stickershop.line-scdn.net/stickershop/v1/sticker/"+sid+ep,
+                 action: {
+                     type: "uri",
+                     uri: "line://shop/sticker/detail/"+pkg}}
+                          ]
+                        }
+        }]).then(function () {
+            liff.closeWindow();
+        });
+    }
+}
+
+function meProfile(){
+    var tipe = getParameterByName('type');
+    liff.getProfile().then(function (prof) {
+        var stat = prof.statusMessage;
+        if (stat == null) {
+            var stat = " - ";
+        }
+        if (stat.length > 60) {
+            var stat = "Status Message is to long! Max 60 words";
+        }
+        if (tipe === 'profile') {
+            liff.sendMessages([{
+                type: "template",
+                altText: "Profile "+prof.displayName,
+                template: {
+                    type: "buttons",
+                    thumbnailImageUrl: prof.pictureUrl,
+                    imageAspectRatio: "square",
+                    imageSize: "cover",
+                    title: prof.displayName,
+                    text: stat,
+                    actions: [
+                        {
+                            type:"uri",
+                            label:"Me",
+                            uri:"line://app/1602687308-GXq4Vvk9?type=profile"
+                        }
+                    ]
+                }
+            }]).then(function () {
+                liff.closeWindow();
+            });
+        }
+    });
 }
